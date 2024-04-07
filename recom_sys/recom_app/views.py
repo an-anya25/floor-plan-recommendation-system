41ca1b507c1eb4002615abcf5ee7d16c6d6155f9
Ananya Babu <ananyababu25@gmail.com>
2024-04-08 02:31:36 +0530

    updated views

--- recom_sys/recom_app/views.py
+++ recom_sys/recom_app/views.py
@@ -11,33 +11,44 @@ import os
 
 
 def recommend_floor_plans_view(request):
-    form = FloorPlanRecommendationForm(request.POST or None)
     recommended_plan_names = []
+    print("1")
+    if request.method == 'POST':
+        print("3")
+        form = FloorPlanRecommendationForm(request.POST)
+        if form.is_valid():
+            print("2")
+            user_input = {
+                "no_of_rooms": form.cleaned_data['no_of_rooms'],
+                "attached_required": form.cleaned_data['attached_required'] == 'yes',
+                "garage_required": form.cleaned_data['garage_required'] == 'yes',
+                "porch_required": form.cleaned_data['porch_required'] == 'yes'
+            }
 
-    if request.method == 'POST' and form.is_valid():
-        user_input = {
-            "no_of_rooms": form.cleaned_data['no_of_rooms'],
-            "attached_required": form.cleaned_data['attached_required'] == 'yes',
-            "garage_required": form.cleaned_data['garage_required'] == 'yes',
-            "porch_required": form.cleaned_data['porch_required'] == 'yes'
-        }
 
+            # Construct the path to data.json relative to the app directory
+            app_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current file (views.py)
+            data_file_path = os.path.join(app_dir, 'data.json')
+            print("app_dir", app_dir)
+            print("data file path", data_file_path)
+            # Load floor plans data from JSON file
+            with open(data_file_path, "r") as file:
+                floor_plans = json.load(file)["floor_plans"]
 
-        # Construct the path to data.json relative to the app directory
-        app_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current file (views.py)
-        data_file_path = os.path.join(app_dir, 'data.json')
-
-        # Load floor plans data from JSON file
-        with open(data_file_path, "r") as file:
-            floor_plans = json.load(file)["floor_plans"]
-
-        # Get the top two recommended floor plan names
-        recommended_plan_names = recommend_floor_plans(user_input, floor_plans)
-        print(recommended_plan_names)
-        # return show_results_view(request, recommended_plan_names)
-        # return redirect('show_results', recommended_plan_names=recommended_plan_names)
-        # Redirect to show_results view with recommended_plan_names as query parameter
-        return HttpResponseRedirect(reverse('show_results') + f"?recommended_plan_names={','.join(recommended_plan_names)}")
+            # Get the top two recommended floor plan names
+            recommended_plan_names = recommend_floor_plans(user_input, floor_plans)
+            print(recommended_plan_names)
+            # return show_results_view(request, recommended_plan_names)
+            # return redirect('show_results', recommended_plan_names=recommended_plan_names)
+            # Redirect to show_results view with recommended_plan_names as query parameter
+            # return HttpResponseRedirect(reverse('show_results') + f"?recommended_plan_names={','.join(recommended_plan_names)}")
+            # return redirect('show_results')
+            redirect_url = reverse('show_results')  + f"?recommended_plan_names={','.join(recommended_plan_names)}"
+            return HttpResponseRedirect(redirect_url)
+        # else:
+        #     print("form not valid")
+    else:
+        form = FloorPlanRecommendationForm()
 
     return render(request, 'user_input_form.html', {'form': form})
 
@@ -45,7 +56,7 @@ def recommend_floor_plans_view(request):
 def show_results_view(request):
     # Retrieve recommended_plan_names from query parameter
     recommended_plan_names = request.GET.get('recommended_plan_names', '').split(',')
-    
+    print(request.GET)
     # Pass the list of recommended plan names to the template
     context = {'recommended_plan_names': recommended_plan_names}
     return render(request, 'show_results.html', context)
